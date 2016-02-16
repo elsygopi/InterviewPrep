da63ed9b45925b592043a25ee0aab99e79cd8a00
EG <gopichaithanya@gmail.com>
2016-02-15 16:46:29 -0800

    Adding Kernighan and Ritchie practice programs project

---
+++ KernighanRitchie/KernighanRitchie/Chapter_01.c
@@ -0,0 +1,24 @@
+//
+//  Chapter_01.c
+//  KernighanRitchie
+//
+//  Created by Gopichaithanya G on 2/15/16.
+//  Copyright © 2016 EG. All rights reserved.
+//
+
+#include "Chapter_01.h"
+
+/* Examples */
+
+void example_01_01() {
+    printf("Example_01_01\n");    
+}
+
+/* Exercises */
+
+void exercise_01_01() {
+    printf("Exercise_01_01\n");
+}
+
+
+/* Other */
---
+++ KernighanRitchie/KernighanRitchie/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  KernighanRitchie
+//
+//  Created by Gopichaithanya G on 2/15/16.
+//  Copyright © 2016 EG. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "Chapter_01.h"
+
+int main() {
+    
+    example_01_01();
+    
+    exercise_01_01();
+    
+    return 0;
+}
