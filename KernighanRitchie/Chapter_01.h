c1b0fa6822f36f99654cd48dc3bf25d0258ac35b
EG <gopichaithanya@gmail.com>
2016-02-15 17:07:27 -0800

    Resolving the directory issues..

---
+++ KernighanRitchie/Chapter_01.h
@@ -0,0 +1,19 @@
+#ifndef Chapter_01_h
+#define Chapter_01_h
+
+#include <stdio.h>
+
+/* Examples */
+
+void example_01_01();
+
+
+/* Exercises */
+
+void exercise_01_01();
+
+
+/* Other */
+
+
+#endif /* Chapter_01_h */
---
+++ KernighanRitchie/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "Chapter_01.h"
+
+int main() {
+    
+//    example_01_01();
+    
+    exercise_01_01();
+    
+    return 0;
+}
