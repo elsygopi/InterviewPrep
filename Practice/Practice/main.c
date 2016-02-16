c397e4905c11e9307b58e606a0b4ac37bfbb6ec6
EG <gopichaithanya@gmail.com>
2016-02-15 16:23:27 -0800

    Committing files from Xcode

---
+++ Practice/Practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Practice
+//
+//  Created by Gopichaithanya G on 2/15/16.
+//  Copyright © 2016 EG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
